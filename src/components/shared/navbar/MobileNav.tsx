104ecaaef1ca49faf12246007bf5f7abfae394e8
Yan Vallejo <yavallejor@gmail.com>
2024-04-08 14:34:57 -0500

    feat: :sparkles: Add Mobile Nav and SearchGlobally Components

---
+++ src/components/shared/navbar/MobileNav.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetClose,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { sidebarLinks } from "@/constants";
+
+const NavContent = () => {
+  const pathName = usePathname();
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((link) => {
+        const isActive =
+          (pathName.includes(link.route) && link.route.length > 1) ||
+          pathName === link.route;
+        return (
+          <SheetClose asChild key={link.route}>
+            <Link
+              href={link.route}
+              key={link.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-lime-900"
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={link.imgURL}
+                width={20}
+                height={20}
+                alt={link.label}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {link.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
+};
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="Menu"
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/images/site-logo.svg"
+            width={40}
+            height={40}
+            alt="DevFlow Logo"
+          />
+          <p className="h2-bold text-dark100_light900 font-spaceGrotesk dark:text-light-900">
+            Dev <span className="text-primary-500">OverFlow</span>
+          </p>
+        </Link>
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+          <SignedOut>
+            <div className="flex flex-col gap-3">
+              <SheetClose asChild>
+                <Link href="/sign-in">
+                  <Button className="small-medium btn-secondary min-h-[41px]  w-full  rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">Sign In</span>
+                  </Button>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link href="/sign-up">
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900  min-h-[41px]  w-full rounded-lg px-4 py-3 shadow-none">
+                    Sign Up
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SignedOut>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
